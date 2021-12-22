031c7ff71250c1582156bbccab84237634302070
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-12-22 02:40:34 -0300

    Updates

--- src/hooks/useGlobal.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import GlobalContext from "../contexts/GlobalContext";
-
-function useGlobal() {
-    return useContext(GlobalContext);
-}
-
-export default useGlobal;
